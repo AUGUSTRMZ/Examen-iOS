a8a98276fceacf5a3cad3abacf026114224d5ed9
AUGUSTRMZ <a01712439@tec.mx>
2025-10-14 18:43:51 -0600

    Pausa para cita con fisioterapeuta, inicie a las 6:09pm y tomo una pausa a las 6:43pm practiamente la aplicacion ya esta funcionando para este punto

---
+++ ExamenArgumentativo/ExamenArgumentativo/Presentation/Views/CountriesListView.swift
@@ -0,0 +1,18 @@
+//
+//  CountriesListView.swift
+//  ExamenArgumentativo
+//
+//  Created by Cesar Augusto  Ramirez Davila on 14/10/25.
+//
+
+import SwiftUI
+
+struct CountriesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountriesListView()
+}
--- ExamenArgumentativo/ExamenArgumentativo/Presentation/Views/CountryListView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CountryListView.swift
-//  ExamenArgumentativo
-//
-//  Created by Cesar Augusto  Ramirez Davila on 14/10/25.
-//
-
-import Foundation
